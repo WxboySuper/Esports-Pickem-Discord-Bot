cc5bfd8c6feb259ca078dcddac0874804f028300
WeatherboySuper <SuperPlays1020@gmail.com>
2025-03-10 17:23:31 -0500

    Fresh Start

--- src/dashboard/routes/__init__.py
+++
@@ -1,3 +0,0 @@
-# File: /discord-esports-pickem/discord-esports-pickem/src/dashboard/routes/__init__.py
-
-# This file can be empty
--- test_startup.py
+++
@@ -1,7 +0,0 @@
-import os
-os.environ['BOT_ENV'] = 'test'
-
-from src.bot.bot import bot, TOKEN
-
-if __name__ == '__main__':
-    bot.run(TOKEN)
